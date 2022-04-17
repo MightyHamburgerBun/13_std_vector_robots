f5824cc3df602eb0469fc1cce5993d74d31cd734
MightyHamburgerBun <mightyhamburgerbun@gmail.com>
2022-04-17 15:28:07 +0300

    fixed

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <string>
+
 int main() {
     int n;
     std::cout << "Number of robots: ";
@@ -21,24 +21,27 @@ int main() {
     std::cout << "Number of operations: ";
     std::cin >> m;
     std::cout << "For placement, use a query like '1 ID'.\n To buy, use a query like '2 index'\n";
-    for (int i = 0; i < m; ++i) {
-        std::string query;
-        std::cin.ignore();
+    std::string query;
+    std::cin.ignore();
+    for (int i = 0; i < m; i++) {
         std::getline(std::cin, query);
-        std::cout << query;
+        //std::cout << "query = " << query << std::endl;
         if (query.length() < 3) {
-            std::cout << "Error1\n";
+            std::cout << "Error\n";
+            i--;
+        } else if (query[1] != ' ') {
+            std::cout << "Error\n";
             i--;
         } else {
             char y = query[0];
             int x = 0;
             int q = 1;
-            for (i = query.length()-1; i > 1; --i) {
+            for (int i = query.length()-1; i > 1; --i) {
                 x += (int)(query[i] - '0') * q;
                 q *= 10;
             }
-            //std::cout << x;
-            std::cout << y;
+            //std::cout << "x = " << x << std::endl;
+            //std::cout << "y = " << y << std::endl;
             if (y == '1') {
                 robots.resize(robots.size() + 1);
                 robots[robots.size() - 1] = x;
@@ -48,15 +51,15 @@ int main() {
                 }
                 robots.resize(robots.size() - 1);
             } else {
-                std::cout << "Error2\n";
+                std::cout << "Error\n";
                 i--;
             }
-            std::cout << "Showcase: { ";
-            for (int i = 0; i < robots.size(); ++i) {
-                std::cout << robots[i];
-                std::cout << " ";
-            }
-            std::cout << "}\n";
         }
+        std::cout << "Showcase: { ";
+        for (int j = 0; j < robots.size(); ++j) {
+            std::cout << robots[j];
+            std::cout << " ";
+        }
+        std::cout << "}\n";
     }
 }
